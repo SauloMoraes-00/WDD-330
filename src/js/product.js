a9eb0d4c1870a160d8141d48785d49e4b1a7d10d
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-05-10 10:00:14 -0300

    first actv

--- src/js/product.js
+++ src/js/product.js
@@ -4,6 +4,18 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  const cartItems = getLocalStorage("so-cart") || [];
+  const existingProduct = cartItems.find((item) => item.Id === product.Id);
+
+  if (existingProduct) {
+    existingProduct.Quantity += 1;
+  } else {
+    product.Quantity = 1;
+    cartItems.push(product);
+  }
+
+  setLocalStorage("so-cart", cartItems);
+  alert(`${product.Name} has been added to your cart.`);
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
