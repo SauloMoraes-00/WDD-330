13a26c2fb6ff7c1a64be1a07dc08fa52f03a351c
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-05-10 10:03:52 -0300

    answer

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  const existingProduct = cartItems.find((item) => item.Id === product.Id);
-
-  if (existingProduct) {
-    existingProduct.Quantity += 1;
-  } else {
-    product.Quantity = 1;
-    cartItems.push(product);
-  }
-
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
-  alert(`${product.Name} has been added to your cart.`);
-  setLocalStorage("so-cart", product);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
