6ad5a9b7cb965382ae80be09a59bd102f59736f8
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-05-17 11:15:04 -0300

    Remove item cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,22 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  document.querySelectorAll(".remove-button").forEach((button) => {
+    button.addEventListener("click", removeFromCart);
+  });
+}
+
+function removeFromCart(event) {
+  const idToRemove = event.target.dataset.id;
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  cartItems = cartItems.filter((item) => item.Id !== idToRemove);
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents(); 
 }
 
 function cartItemTemplate(item) {
@@ -20,9 +33,11 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="remove-button" data-id="${item.Id}">Remove</button>
 </li>`;
 
   return newItem;
 }
 
 renderCartContents();
+
