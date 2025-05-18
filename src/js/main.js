c1b5bc4597712bae45ec61a64d32b7d8b3dcbece
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-05-18 16:49:34 -0300

    save dist

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData("./json/tents");
+console.log(`dataSource`);
+
+console.log(dataSource);
+
 
 const element = document.querySelector(".product-list");
 
