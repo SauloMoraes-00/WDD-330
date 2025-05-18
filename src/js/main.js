4a5765be45c729f685afda086a7c73610b14c419
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-05-18 16:54:04 -0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("./json/tents");
+const dataSource = new ProductData("tents");
 console.log(`dataSource`);
 
 console.log(dataSource);
