ec010348626fbcbaf7651d7bb514aa2d7adf05d1
SauloMoraes-00 <saulo.moraesoficial@gmail.com>
2025-05-22 16:17:04 -0300

    x and color

--- src/js/cart.js
+++ src/js/cart.js
@@ -33,7 +33,7 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
-  <button class="remove-button" data-id="${item.Id}">Remove</button>
+  <button class="remove-button" data-id="${item.Id}">❌</button>
 </li>`;
 
   return newItem;
